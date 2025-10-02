b9ab98268cba4f262899944ef99a926286beccf2
Mads Wrang <madscwn@gmail.com>
2025-10-02 19:27:33 +0200

    add rust nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+          openssl
+        ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+    };
+}
